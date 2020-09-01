2569b1b8bef0433e2e924e98ad09dcd38828597e
Joshua Wu <68614415+joshhasdysentery@users.noreply.github.com>
2020-09-01 02:02:39 -0400

    adding project files

---
+++ App.js
@@ -0,0 +1,19 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import Header from './components/Header';
+
+export default function App() {
+  return (
+    <View style={styles.screen}>
+      <Header />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+  },
+});
