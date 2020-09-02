a7c885ba64e39fae575d5acfdca2b1ffab3e2e57
Joshua Wu <68614415+joshhasdysentery@users.noreply.github.com>
2020-09-01 23:28:59 -0400

    Update App.js

--- App.js
+++ App.js
@@ -3,11 +3,13 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import Header from './components/Header';
+import NewsScreen from './screens/NewsScreen';
 
 export default function App() {
   return (
     <View style={styles.screen}>
       <Header />
+      <NewsScreen />
     </View>
   );
 }
