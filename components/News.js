070326291a174199adeb45959a9e839dab652242
Joshua Wu <68614415+joshhasdysentery@users.noreply.github.com>
2020-09-21 14:56:09 -0400

    Delete News.js

--- components/News.js
+++
@@ -1,7 +0,0 @@
-const url =
-  'http://newsapi.org/v2/everything?q=bitcoin&from=2020-08-02&sortBy=publishedAt&apiKey=c782072813894c54847457bfe59eca83';
-
-export default function getNews() {
-    let result = await fetch(url).then(response => response.json());
-    return result.articles;
-}
